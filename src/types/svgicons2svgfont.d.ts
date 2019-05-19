510106d1a4d6f81394622519c385320cf28fb2af
Zhongqc <zhongqc7@gmail.com>
2019-05-19 14:03:50 +0800

    library types declaration

---
+++ src/types/svgicons2svgfont.d.ts
@@ -0,0 +1,37 @@
+export = index;
+declare class index {
+  constructor (options: any);
+  glyphs: any;
+  log: any;
+  addListener (ev: any, fn: any): any;
+  cork (): void;
+  destroy (err: any, cb: any): any;
+  emit (type: any, args: any): any;
+  end (chunk?: any, encoding?: any, cb?: any): any;
+  eventNames (): any;
+  getMaxListeners (): any;
+  isPaused (): any;
+  listenerCount (type: any): any;
+  listeners (type: any): any;
+  off (type: any, listener: any): any;
+  on (ev: any, fn: any): any;
+  once (type: any, listener: any): any;
+  pause (): any;
+  pipe (dest: any, pipeOpts?: any): any;
+  prependListener (type: any, listener: any): any;
+  prependOnceListener (type: any, listener: any): any;
+  push (chunk: any, encoding: any): any;
+  rawListeners (type: any): any;
+  read (n: any): any;
+  removeAllListeners (ev: any): any;
+  removeListener (ev: any, fn: any): any;
+  resume (): any;
+  setDefaultEncoding (encoding: any): any;
+  setEncoding (enc: any): any;
+  setMaxListeners (n: any): any;
+  uncork (): void;
+  unpipe (dest: any): any;
+  unshift (chunk: any): any;
+  wrap (stream: any): any;
+  write (chunk: any, encoding?: any, cb?: any): any;
+}
