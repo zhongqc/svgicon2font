d995a0659803a40e0102497da010e11e5b25a6c1
Zhongqc <zhongqc7@gmail.com>
2019-06-16 17:37:29 +0800

    v1.0.2 build result

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,2 @@
 import { SVGIcon2FontConfig } from './build-config';
-export declare function svgIcon2Font(initinalConfig: SVGIcon2FontConfig): void;
+export declare function svgIcon2Font(initinalConfig: SVGIcon2FontConfig): Promise<void>;
